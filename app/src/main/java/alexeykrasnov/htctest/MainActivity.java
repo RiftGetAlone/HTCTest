8f2288812f5afc4b70c056850783b0206e10a2db
RiftGetAlone <fort.kto2@gmail.com>
2018-06-10 20:13:56 +0300

    update

--- app/src/main/java/alexeykrasnov/htctest/MainActivity.java
+++ app/src/main/java/alexeykrasnov/htctest/MainActivity.java
@@ -1,12 +1,13 @@
 package alexeykrasnov.htctest;
 
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,6 +20,8 @@ import com.google.gson.GsonBuilder;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView employeesList;
+    private LinearLayout linearLayout;
+    private Button retryButton;
     private TextView companyAge;
     private TextView competences;
     private static final String URL = "http://www.mocky.io/v2/56fa31e0110000f920a72134";
@@ -27,12 +30,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        linearLayout = findViewById(R.id.linear_layout);
+        retryButton = new Button(this);
         companyAge = findViewById(R.id.tvCompanyAge);
         competences = findViewById(R.id.tvCompetences);
         employeesList = findViewById(R.id.employees_list);
+
         employeesList.setLayoutManager(new LinearLayoutManager(this));
 
-        StringRequest request = new StringRequest(URL, new Response.Listener<String>() {
+        final StringRequest request = new StringRequest(URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 GsonBuilder gsonBuilder = new GsonBuilder();
@@ -47,10 +54,23 @@ public class MainActivity extends AppCompatActivity {
             }
         }, new Response.ErrorListener() {
             @Override public void onErrorResponse(VolleyError error) {
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(200,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+
                 companyAge.setText(R.string.network_error);
+                retryButton.setText(R.string.retry);
+                linearLayout.addView(retryButton, lp);
             }
         });
-        RequestQueue queue = Volley.newRequestQueue(this);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.removeView(v);
+                queue.add(request);
+            }
+        };
+        retryButton.setOnClickListener(onClickListener);
         queue.add(request);
     }
 }
